a24aa0ce4b1ba3875ef8c1df5b1b3c7c8ed07008
Ovosh <32675978+vlplastelin@users.noreply.github.com>
2025-09-06 19:51:20 +0300

    Update test.js

--- test.js
+++ test.js
@@ -201,6 +201,7 @@ function sendMessage(json, toId = null, excludeId = null) {
   if (typeof window.rtc._dataChannels === 'object') {
     console.log("Still sending as a host")
     const channels = window.rtc._dataChannels;
+    console.log(Object.entries(channels))
     Object.entries(channels).forEach(([id, ch]) => {
       console.log(`Data channel state for client ${id}:`, ch.readyState);
       if (excludeId && id === excludeId) return;
